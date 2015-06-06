b82d9f650a37ff6d5606a2276353b59df3eaea70
Antonio Cunei <antonio.cunei@typesafe.com>
2015-06-06 11:11:48 +0200

    Refactored testing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,4 +6,7 @@ object Dependencies {
   lazy val scala211 = "2.11.6"
 
   lazy val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
+
+  val scalaCheck           = "org.scalacheck" %% "scalacheck" % "1.11.5"
+  val specs2               = "org.specs2" %% "specs2" % "2.3.11"
 }
