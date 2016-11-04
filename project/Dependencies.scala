8a99c32cc938c8b41f2ee98cc158cf47b6d66327
kenji yoshida <6b656e6a69@gmail.com>
2016-11-04 17:40:48 +0900

    Scala 2.12.0 final, scalacheck 1.13.4 (#27)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ import Keys._
 object Dependencies {
   val scala210 = "2.10.6"
   val scala211 = "2.11.8"
-  val scala212 = "2.12.0-RC1"
+  val scala212 = "2.12.0"
 
   val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
 
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.2"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.0"
 }
