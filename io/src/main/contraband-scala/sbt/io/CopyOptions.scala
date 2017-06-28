c0fd090fc3fcecb5da4b7c646d8a51b171934948
Dale Wijnand <dale.wijnand@gmail.com>
2017-06-28 14:58:31 +0100

    Add option preserveExecutable to IO.copyFile and IO.copyDirectory
    Fixes sbt/sbt#944
    
    Modified forward port of
    https://github.com/sbt/sbt/commit/1e61b9acf34b7d89a54a5adc021cc37cd3ecd0ef
    
    Modified because it switches to using a CopyOptions contraband record
    and overloading.

---
+++ io/src/main/contraband-scala/sbt/io/CopyOptions.scala
@@ -0,0 +1,50 @@
+/**
+ * This code is generated using [[http://www.scala-sbt.org/contraband/ sbt-contraband]].
+ */
+
+// DO NOT EDIT MANUALLY
+package sbt.io
+/** The options for the copy operation in `IO`. */
+final class CopyOptions private (
+  /**
+   * A source file is always copied if `overwrite` is true.
+   * If `overwrite` is false, the source is only copied if the target is missing or is older than the
+   * source file according to last modified times.
+   * If the source is a directory, the corresponding directory is created.
+   */
+  val overwrite: Boolean,
+  /** If `true` the last modified times are copied. */
+  val preserveLastModified: Boolean,
+  /** If `true` the executable properties are copied. */
+  val preserveExecutable: Boolean) extends Serializable {
+  
+  private def this() = this(false, false, true)
+  
+  override def equals(o: Any): Boolean = o match {
+    case x: CopyOptions => (this.overwrite == x.overwrite) && (this.preserveLastModified == x.preserveLastModified) && (this.preserveExecutable == x.preserveExecutable)
+    case _ => false
+  }
+  override def hashCode: Int = {
+    37 * (37 * (37 * (37 * (17 + "CopyOptions".##) + overwrite.##) + preserveLastModified.##) + preserveExecutable.##)
+  }
+  override def toString: String = {
+    "CopyOptions(" + overwrite + ", " + preserveLastModified + ", " + preserveExecutable + ")"
+  }
+  protected[this] def copy(overwrite: Boolean = overwrite, preserveLastModified: Boolean = preserveLastModified, preserveExecutable: Boolean = preserveExecutable): CopyOptions = {
+    new CopyOptions(overwrite, preserveLastModified, preserveExecutable)
+  }
+  def withOverwrite(overwrite: Boolean): CopyOptions = {
+    copy(overwrite = overwrite)
+  }
+  def withPreserveLastModified(preserveLastModified: Boolean): CopyOptions = {
+    copy(preserveLastModified = preserveLastModified)
+  }
+  def withPreserveExecutable(preserveExecutable: Boolean): CopyOptions = {
+    copy(preserveExecutable = preserveExecutable)
+  }
+}
+object CopyOptions {
+  
+  def apply(): CopyOptions = new CopyOptions(false, false, true)
+  def apply(overwrite: Boolean, preserveLastModified: Boolean, preserveExecutable: Boolean): CopyOptions = new CopyOptions(overwrite, preserveLastModified, preserveExecutable)
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+addSbtPlugin("org.scala-sbt" % "sbt-houserules" % "0.3.3")
+addSbtPlugin("org.scala-sbt" % "sbt-contraband" % "0.3.0-M6")
