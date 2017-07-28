e568dfd3d9e8b058f5d13874dbf6bd591153e5de
Dale Wijnand <dale.wijnand@gmail.com>
2017-07-28 07:55:33 +0100

    Upgrade to Scala 2.12.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   val scala210 = "2.10.6"
   val scala211 = "2.11.11"
-  val scala212 = "2.12.2"
+  val scala212 = "2.12.3"
   val scala213 = "2.13.0-M1"
 
   val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
