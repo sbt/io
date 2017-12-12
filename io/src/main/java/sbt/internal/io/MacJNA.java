867da5178c1c4db423973116a16dd37edfd79e61
Antonio Cunei <antonio.cunei@typesafe.com>
2017-12-12 04:10:27 +0100

    Replace utimensat with direct Darwin calls on MacOS
    The `utimensat` call is only available in High Sierra, but not
    on previous versions of OSX. An alternative could be to use
    utimes, but that call always changes both the modification as
    well as the last access time, requiring an additional trip
    to `stat` in order to reconstruct the necessary information.
    
    A better alternative is just calling the native interface,
    similarly to the Windows interface code. This pull request
    adds direct support for precise timestamps on the Mac by
    using efficient Darwin calls.

---
+++ io/src/main/java/sbt/internal/io/MacJNA.java
@@ -0,0 +1,51 @@
+package sbt.internal.io;
+
+import com.sun.jna.Structure;
+import java.util.List;
+import java.util.Arrays;
+
+public class MacJNA {
+
+  public static class Attrlist extends Structure {
+    public short bitmapcount;
+    public short reserved;
+    public int commonattr;
+    public int volattr;
+    public int dirattr;
+    public int fileattr;
+    public int forkattr;
+    @Override
+    public List<String> getFieldOrder() {
+      return Arrays.asList(new String[]{
+        "bitmapcount", "reserved", "commonattr",
+        "volattr", "dirattr", "fileattr", "forkattr"
+      });
+    };
+  }
+
+  public static class Timespec extends Structure {
+    public long tv_sec;
+    public long tv_nsec;
+    @Override
+    public List<String> getFieldOrder() {
+      return Arrays.asList(new String[]{
+        "tv_sec", "tv_nsec"
+      });
+    };
+  }
+
+  public static class TimeBuf extends Structure {
+    public int size;
+    public long tv_sec;
+    public long tv_nsec;
+    @Override
+    public List<String> getFieldOrder() {
+      return Arrays.asList(new String[]{
+        "size", "tv_sec", "tv_nsec"
+      });
+    };
+    public TimeBuf() {
+      setAlignType(Structure.ALIGN_NONE);
+    }
+  }
+}
