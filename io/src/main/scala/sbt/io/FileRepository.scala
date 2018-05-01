2e311993d5cd7ada59b99b7c0eb93101c8573601
Ethan Atkins <ethan.atkins@gmail.com>
2018-04-30 18:33:17 -0700

    Add FileRepository to io
    Throughout the sbt code base, it is necessary to list the contents of a
    directory. For large projects, this can become slow because it requires
    a lot of file I/O and also may perform computationally expensive work
    like computing the sha1 sum of the file contents. It can then be a big
    performance win to introduce an in-memory cache of the file system that
    is updated as the underlying file contents change.
    
    In this commit, I add three traits that will be used to optimize file
    system access:
    
    1) TypedPath -- represents a path on the file system, but may cache some
       of the attributes of the file, e.g. whether or not the file is a
       directory, so that filters may be applied without necessarily having
       to query the file system. Note that the equivalent apis in
       java.io.File or java.nio.file.Files must stat the file which is an
       expensive io operation.
    
    2) FileTreeView -- this provides a simple api for listing the contents
       of a directory. Unlike, say, java.nio.file.Files.walk, the
       FileTreeView.list method returns TypedPath instances, allowing for
       fast filtering.
    
    3) FileTreeDataView -- augments FileTreeView with a listEntries method
       that returns instances of Entry[T], which augment TypedPath with a
       data value. For example, the Entry.value may be the sha-1 sum of a
       source file or it could be the last modified time. Regardless, the
       idea is that it allows the implementation to optionally cache this
       value so that it does not need to do any file system operations to
       return the value to the user. I added this specifically so that I
       could add a zinc external hook in sbt that allows zinc to check which
       source files have changed without having to re-compute these values,
       which is generally bound by disk io, not cpu.
    
    4) FileRepository -- augments FileTreeDataView with the option of
       registering paths for monitoring and adding observers that are
       notified whenever the monitored paths are updated. The repository
       specification does not require caching of the Entry values, though it
       is generally implied.
    
    These traits mirror the
    com.swoval.files.{ FileTreeView, FileTreeDataView, FileTreeRepository, TypedPath}
    interfaces defined in the swoval file-tree-views library. I do not want
    to directly expose the swoval apis to sbt users hence I define the
    shadow api. Subsequent commits will provide implementations of these
    interfaces.

---
+++ io/src/main/scala/sbt/io/FileRepository.scala
@@ -0,0 +1,235 @@
+package sbt.io
+
+import java.io.IOException
+import java.nio.file.{ Path => JPath }
+
+import sbt.io.FileTreeDataView.{ Entry, Observable }
+
+/**
+ * Represents a path in the file system. It may cache some of the file attributes so that no disk
+ * io is necessary to check, for example, whether the file is a directory or regular file.
+ */
+trait TypedPath {
+
+  /**
+   * The underlying path that this represents.
+   * @return the path
+   */
+  def getPath: JPath
+
+  /**
+   * Indicates whether or not the file exists. Because it may be cached, this method may not
+   * represent the current state of the file.
+   * @return true if the file exists
+   */
+  def exists: Boolean
+
+  /**
+   * Indicates whether or not the file is a directory. Because it may be cached, this method may not
+   * represent the current state of the file.
+   * @return true if the file is a directory
+   */
+  def isDirectory: Boolean
+
+  /**
+   * Indicates whether or not the file is a regular file. Because it may be cached, this method may not
+   * represent the current state of the file.
+   * @return true if the file is a regular file
+   */
+  def isFile: Boolean
+
+  /**
+   * Indicates whether or not the file is a symbolic link. Because it may be cached, this method may not
+   * represent the current state of the file.
+   * @return true if the file is a symbolic link
+   */
+  def isSymbolicLink: Boolean
+
+  /**
+   * Returns the real path of the file. If the file is not a symbolic link, it should return the
+   * target, if it exists. Otherwise it should return the path itself.
+   * @return either the path itself or the target of the symbolic link if the file is a symbolic
+   *         link and the target exists
+   */
+  def toRealPath: JPath
+
+  override def toString: String = s"TypedPath($getPath)"
+}
+
+/**
+ * Provides a view into the file system that allows retrieval of the children of a particular path.
+ * Specific implementations may or may not use a cache for retrieval.
+ */
+trait FileTreeView extends AutoCloseable {
+
+  /**
+   * List the contents of the current directory.
+   *
+   * @param path      the path to list
+   * @param maxDepth  controls the depth of children of the path to include in the results. When
+   *                  maxDepth is -1, [[list]] should only return the TypedPath for this directory.
+   *                  For non-negative values, [[list]] should return only entries whose relativized
+   *                  path has {{{maxDepth - 1}}} elements. For example, when maxDepth is zero, all of
+   *                  the children of the path should be included in the result, but none of the
+   *                  children of any of the subdirectories should be incldued.
+   * @param filter    only return files accepted by the filter
+   * @return a sequence of [[TypedPath]]s.
+   */
+  def list(path: JPath, maxDepth: Int, filter: TypedPath => Boolean): Seq[TypedPath]
+}
+
+object FileTreeView {
+  object AllPass extends (TypedPath => Boolean) {
+    override def apply(e: TypedPath): Boolean = true
+  }
+}
+
+/**
+ * Provides a view into the file system that allows retrieval of the children of a particular path
+ * where each child has an associated [[Entry]] that is generally derived from the file (e.g. the
+ * [[Entry.value]] may be the md5 hash of the underlying file). Specific implementations may or may
+ * not use a cache for retrieval. It extends FileTreeView since [[FileTreeView.list]] can be
+ * trivially implemented using `listEntries`.
+ */
+trait FileTreeDataView[+T] extends FileTreeView with AutoCloseable {
+
+  /**
+   * List the contents of the current directory where each returned [[Entry]] has a data value
+   * associated with it.
+   *
+   * @param path      the path to list
+   * @param maxDepth  controls the depth of children of the path to include in the results. When
+   *                  maxDepth is -1, [[listEntries]] should only return the TypedPath for this
+   *                  directory. For non-negative values, [[listEntries]] should return only
+   *                  entries whose relativized path has `maxDepth - 1` elements. For example,
+   *                  when maxDepth is zero, all of the children of the path should be included in
+   *                  the result, but none of the children of any of the subdirectories should be
+   *                  included.
+   * @param filter    only return files accepted by the filter
+   * @return a sequence of [[Entry]] instances.
+   */
+  def listEntries(path: JPath, maxDepth: Int, filter: Entry[T] => Boolean): Seq[Entry[T]]
+}
+
+object FileTreeDataView {
+  abstract case class Entry[+T](path: JPath, value: Either[IOException, T]) extends TypedPath {
+    override def toString: String = s"Entry($path, $value)"
+  }
+
+  /**
+   * A FileRepository observer that receives callbacks
+   * @tparam T the generic type of [[Entry.value]] instances for the [[FileRepository]]
+   */
+  trait Observer[-T] {
+
+    /**
+     * Process a newly created entry
+     * @param newEntry the newly acceptcreate entry
+     */
+    def onCreate(newEntry: Entry[T]): Unit
+
+    /**
+     * Process a deleted entry
+     * @param oldEntry the deleted entry
+     */
+    def onDelete(oldEntry: Entry[T]): Unit
+
+    /**
+     * Process a deleted entry
+     * @param oldEntry the previous entry
+     * @param newEntry the current entry
+     */
+    /*
+     * Note that this interface exists for documentation purposes so that it is clear that the
+     * deleted entry is on the left. This would not be clear if filtering was handled with a
+     * `(Entry[T], Entry[T]) => Boolean`.
+     */
+    def onUpdate(oldEntry: Entry[T], newEntry: Entry[T]): Unit
+  }
+  object Observer {
+
+    /**
+     * Create a new Observer from callback functions.
+     *
+     * @param onCreate the callback to invoke when a new entry is created
+     * @param onDelete the callback to invoke when an entry is
+     * @param onUpdate the callback to invoke when an entry is updated. Note that the previous
+     *                 entry must be the first argument to this function.
+     * @tparam T the type of [[Entry.value]] instances for the [[Observable]]
+     * @return an [[Observer]] with the provided callbacks.
+     */
+    def apply[T](onCreate: Entry[T] => Unit,
+                 onDelete: Entry[T] => Unit,
+                 onUpdate: (Entry[T], Entry[T]) => Unit): Observer[T] =
+      new Impl(onCreate, onDelete, onUpdate)
+
+    private[FileTreeDataView] class Impl[-T](oncreate: Entry[T] => Unit,
+                                             ondelete: Entry[T] => Unit,
+                                             onupdate: (Entry[T], Entry[T]) => Unit)
+        extends Observer[T] {
+      override def onCreate(newEntry: Entry[T]): Unit = oncreate(newEntry)
+      override def onDelete(oldEntry: Entry[T]): Unit = ondelete(oldEntry)
+      override def onUpdate(oldEntry: Entry[T], newEntry: Entry[T]): Unit =
+        onupdate(oldEntry, newEntry)
+    }
+  }
+
+  /**
+   * An object that monitors a file system. The interface is very similar to that provided by other
+   * libraries/frameworks, such as [[http://reactivex.io/intro.html rxJava]]. When it detects changes
+   * in the file system, it will invoke a set of user specified callbacks. The Observable also
+   * allows the user to add and removes paths to monitor.
+   *
+   * @tparam T the generic type of [[Entry.value]] instances
+   */
+  trait Observable[+T] extends AutoCloseable {
+
+    /**
+     * Add callbacks to be invoked on file events.
+     *
+     * @param observer the callbacks to invoke
+     * @return a handle to the callback.
+     */
+    def addObserver(observer: Observer[T]): Int
+
+    /**
+     * Removes a callback that was added via [[addObserver]]
+     *
+     * @param handle The handle returned by [[addObserver]]
+     */
+    def removeObserver(handle: Int): Unit
+  }
+}
+
+/**
+ * Monitors registered directories for file changes. A typical implementation will keep an
+ * in memory cache of the file system that can be queried in [[FileRepository#listEntries]]. The
+ * [[FileRepository#register]] method adds monitoring for a particular cache. A filter may be provided
+ * so that the cache doesn't waste memory on files the user doesn't care about. The
+ * cache may be shared across a code base so there additional apis for adding filters or changing
+ * the recursive property of a directory.
+ *
+ * @tparam T the type of the [[Entry.value]]s.
+ */
+trait FileRepository[+T] extends Observable[T] with FileTreeDataView[T] with AutoCloseable {
+
+  /**
+   * Register a directory for monitoring
+   *
+   * @param path     the path to list
+   * @param maxDepth controls how the depth of children of the registered path to consider. When
+   *                 maxDepth is -1, then the repository should only monitor the path itself. When
+   *                 it is zero, the the repository should monitor the path and its direct children.
+   *                 For values greater than zero,
+   * @return an Either that is a Right when register has no errors and a Left if an IOException is
+   *         thrown while registering the path. The result should be true if the path has
+   *         never been previously registered or if the recursive flag flips from false to true.
+   */
+  def register(path: JPath, maxDepth: Int): Either[IOException, Boolean]
+
+  /**
+   * Remove a path from monitoring.
+   * @param path the path to stop monitoring
+   */
+  def unregister(path: JPath): Unit
+}
