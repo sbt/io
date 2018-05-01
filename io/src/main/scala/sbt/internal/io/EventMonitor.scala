d8e3c130e86b8e9a94309e0def9569db9e0841e5
Ethan Atkins <ethan.atkins@gmail.com>
2018-04-30 18:32:12 -0700

    Refactor EventMonitor to use a FileEventMonitor
    In previous commits, I add the Observable trait along with the
    FileEventMonitor trait. These provide the fundamental building blocks
    that can be used to implement the simpler EventMonitor trait. To
    increase the generality of the provided methods, I add the
    WatchServiceBackedObservable class. This implements the Observable
    interface mostly using the implementation provided in the
    EventMonitorImpl class. Because the implementation logic is largely the
    same and the EventMonitor interface is easily expressed using the
    FileEventMonitor interface, I was able to replace the EventMonitor apply
    method implementation.
    
    As a stand-alone commit, there is not much point to this change. The
    WatchServiceBackedObservable is, however, a more useful class than the
    EventMonitorImpl and will find use in subsequent commits that replace
    the EventMonitor entirely.
    
    One important implementation change though is that there is no longer a
    separate thread for polling System.in. The EventMonitor
    terminationCondition is now applied by polling the FileEventMonitor for
    small periods (i.e. 10.milliseconds) and checking System.in between
    polls. This not only reduces the bookkeeping associated with managing
    that thread, but it also fixes a bug where in sbt the '~console' command
    does not work because the user input thread will detect any input during
    the console session (which is sure to include an <enter>). There is
    still a background thread for polling the underlying WatchService for
    events and invoking the observer callbacks when events are detected.

--- io/src/main/scala/sbt/internal/io/EventMonitor.scala
+++ io/src/main/scala/sbt/internal/io/EventMonitor.scala
@@ -1,17 +1,11 @@
 package sbt.internal.io
 
-import java.io.IOException
-import java.nio.file.StandardWatchEventKinds.OVERFLOW
 import java.nio.file._
-import java.nio.file.attribute.BasicFileAttributes
-import java.util.concurrent.ArrayBlockingQueue
-import java.util.concurrent.atomic.{ AtomicBoolean, AtomicInteger }
 
-import sbt.io.WatchService
+import sbt.io
+import sbt.io.{ Path => _, _ }
 
 import scala.annotation.tailrec
-import scala.collection.JavaConverters._
-import scala.collection.mutable
 import scala.concurrent.duration._
 
 /**
@@ -40,49 +34,16 @@ private[sbt] sealed trait EventMonitor extends AutoCloseable {
  * Provides factory methods for creating instances of EventMonitor.
  */
 private[sbt] object EventMonitor {
-  private sealed trait Event
-  private case object Cancelled extends Event
-  private case class Triggered(path: Path) extends Event
-
-  private class EventMonitorImpl private[EventMonitor] (
-      private[this] val service: WatchService,
-      private[this] val events: ArrayBlockingQueue[Event],
-      private[this] val eventThread: Looper with HasWatchState,
-      private[this] val userInputThread: Looper,
-      private[this] val logger: Logger,
-      private[this] val closeService: Boolean)
-      extends EventMonitor {
-
-    override def state(): WatchState = eventThread.state()
-
-    override def awaitEvent(): Boolean = events.take() match {
-      case Cancelled => false
-      case Triggered(path) =>
-        logger.debug(s"Triggered watch event due to updated path: $path")
-        eventThread.incrementCount()
-        true
-    }
-
-    override def close(): Unit = {
-      if (closed.compareAndSet(false, true)) {
-        if (closeService) service.close()
-        userInputThread.close()
-        eventThread.close()
-        logger.debug("Closed EventMonitor")
-      }
-    }
-
-    private[this] val closed = new AtomicBoolean(false)
-  }
 
   /**
    * Create a new EventMonitor
-   * @param state The initial watch state for the monitor
-   * @param delay Maximum duration that the monitor will poll the watch service for events
-   * @param antiEntropy Minimum duration that must elapse before a build may by re-triggered by
-   *                    the same file
+   *
+   * @param state                The initial watch state for the monitor
+   * @param delay                Maximum duration that the monitor will poll the watch service for events
+   * @param antiEntropy          Minimum duration that must elapse before a build may by re-triggered by
+   *                             the same file
    * @param terminationCondition Exits watch if evaluates to true
-   * @param logger
+   * @param logger               Logs output
    * @return The new EventMonitor
    */
   def apply(state: WatchState,
@@ -92,215 +53,58 @@ private[sbt] object EventMonitor {
             logger: Logger = NullLogger): EventMonitor =
     applyImpl(state, delay, antiEntropy, terminationCondition, logger, closeService = true)
 
-  private[EventMonitor] def applyImpl(state: WatchState,
-                                      delay: FiniteDuration,
-                                      antiEntropy: FiniteDuration,
-                                      terminationCondition: => Boolean,
-                                      logger: Logger,
-                                      closeService: Boolean): EventMonitor = {
-    val events = new ArrayBlockingQueue[Event](1)
-    val eventThread = newEventsThread(delay, antiEntropy, state, events, logger)
-    val userInputThread = newUserInputThread(terminationCondition, events, logger)
-    new EventMonitorImpl(state.service, events, eventThread, userInputThread, logger, closeService)
-  }
-
   private[io] def legacy(state: WatchState,
                          delay: FiniteDuration,
                          terminationCondition: => Boolean): EventMonitor =
     applyImpl(state, delay, 40.milliseconds, terminationCondition, NullLogger, closeService = false)
 
-  private trait HasWatchState {
-    def state(): WatchState
-    def incrementCount(): Unit
-  }
-  private def newEventsThread(delay: FiniteDuration,
-                              antiEntropy: FiniteDuration,
-                              s: WatchState,
-                              events: ArrayBlockingQueue[Event],
-                              logger: Logger): Looper with HasWatchState = {
-    var recentEvents = Map.empty[Path, Deadline]
-    var registered = s.registered
-    var count = s.count
-    val lock = new Object
-    new Looper(s"watch-state-event-thread-${eventThreadId.incrementAndGet()}") with HasWatchState {
-      def incrementCount(): Unit = lock.synchronized { count += 1 }
-      def state(): WatchState = lock.synchronized(s.withCount(count).withRegistered(registered))
-      override def loop(): Unit = {
-        recentEvents = recentEvents.filterNot(_._2.isOverdue)
-        getFilesForKey(s.service.poll(delay)).foreach(maybeTrigger)
-      }
-      def getFilesForKey(key: WatchKey): Vector[Path] = key match {
-        case null => Vector.empty
-        case k =>
-          val rawEvents = k.synchronized {
-            val events = k.pollEvents.asScala.toVector
-            k.reset()
-            events
-          }
-          val keyPath = k.watchable.asInstanceOf[Path]
-          val allEvents = rawEvents.flatMap {
-            case e if e.kind.equals(OVERFLOW) =>
-              handleOverflow(k)
-            case e if !e.kind.equals(OVERFLOW) && e.context != null =>
-              Some(keyPath.resolve(e.context.asInstanceOf[Path]))
-            case _ => None
-          }
-          logger.debug(s"Received events:\n${allEvents.mkString("\n")}")
-          val (exist, notExist) = allEvents.partition(Files.exists(_))
-          val (updatedDirectories, updatedFiles) = exist.partition(Files.isDirectory(_))
-          val newFiles = updatedDirectories.flatMap(filesForNewDirectory)
-          lock.synchronized { registered --= notExist }
-          notExist.foreach(s.unregister)
-          updatedFiles ++ newFiles ++ notExist
+  private[EventMonitor] def applyImpl(watchState: WatchState,
+                                      delay: FiniteDuration,
+                                      antiEntropy: FiniteDuration,
+                                      terminationCondition: => Boolean,
+                                      logger: Logger,
+                                      closeService: Boolean): EventMonitor = {
+    val eventLogger = new io.Logger {
+      override def debug(msg: => Any): Unit = logger.debug(msg)
+    }
+    val observable = new WatchServiceBackedObservable[Path](watchState,
+                                                            delay,
+                                                            (_: TypedPath).getPath,
+                                                            closeService,
+                                                            eventLogger)
+    val monitor = FileEventMonitor.antiEntropy(observable, antiEntropy, eventLogger)
+    new EventMonitor {
+      private[this] var count = watchState.count
+
+      /** Block indefinitely until the monitor receives a file event or the user stops the watch. */
+      @tailrec
+      override final def awaitEvent(): Boolean = {
+        val triggeredPath = monitor
+          .poll(10.millis)
+          .find(p => watchState.sources.exists(s => s.accept(p.entry.typedPath.getPath)))
+        triggeredPath match {
+          case Some(p) =>
+            logger.debug(s"Triggered by ${p.entry.typedPath.getPath}")
+            count += 1
+            true
+          case _ if terminationCondition => false
+          case _                         => awaitEvent()
+        }
       }
 
-      /*
-       * In the case of an overflow, we must poll the file system to find out if there are added
-       * or removed directories. When there are new directories, we also want to return file
-       * events for the files that are found therein. Because an overflow is likely to occur while
-       * a directory is still being modified, we poll repeatedly until we get the same list of
-       * files consecutively. We will not trigger for any files that are updated while the WatchKey
-       * is in the OVERFLOW state. There is no good way to fix this without caching mtimes for
-       * all of the files, which I don't think is worth doing at this juncture.
-       */
-      private def handleOverflow(key: WatchKey): Vector[Path] = lock.synchronized {
-        val allFiles = new mutable.HashSet[Path]
-        def addNewFiles(): Unit = {
-          allFiles.clear()
-          val path = key.watchable.asInstanceOf[Path]
-          Files.walkFileTree(
-            path,
-            new FileVisitor[Path] {
-              override def preVisitDirectory(dir: Path,
-                                             attrs: BasicFileAttributes): FileVisitResult = {
-                allFiles += dir
-                if (!registered.contains(dir)) registered += dir -> s.register(dir)
-                FileVisitResult.CONTINUE
-              }
-              override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
-                allFiles += file
-                FileVisitResult.CONTINUE
-              }
-              override def visitFileFailed(file: Path, exc: IOException): FileVisitResult =
-                FileVisitResult.SKIP_SUBTREE
-              override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult =
-                FileVisitResult.CONTINUE
-            }
-          )
-          ()
-        }
+      /** A snapshot of the WatchState that includes the number of build triggers and watch sources. */
+      override def state(): WatchState = watchState.withCount(count)
 
-        var oldFiles = mutable.Set.empty[Path]
-        do {
-          oldFiles = allFiles
-          addNewFiles()
-        } while (oldFiles != allFiles)
-        registered --= registered.collect {
-          case (d, k) if !Files.exists(d) =>
-            k.reset()
-            k.cancel()
-            d
-        }
-        allFiles.toVector
-      }
-
-      /*
-       * Returns new files found in new directory and any subdirectories, assuming that there is
-       * a recursive source with a base that is parent to the directory.
-       */
-      private def filesForNewDirectory(dir: Path): Iterator[Path] = {
-        lazy val recursive =
-          s.sources.exists(src => dir.startsWith(src.base.toPath) && src.recursive)
-        if (!registered.contains(dir) && recursive) {
-          val dirs = Files.walk(dir).iterator.asScala.filter(Files.isDirectory(_))
-          val newDirs = dirs.map(d => d -> s.register(d)).toIndexedSeq
-          lock.synchronized { registered ++= newDirs }
-          Files.walk(dir).iterator.asScala
-        } else Nil.iterator
-      }
-      /*
-       * Triggers only if there is no pending Trigger and the file is not in an anti-entropy
-       * quarantine.
-       */
-      private def maybeTrigger(path: Path): Unit =
-        if (s.accept(path)) {
-          if (recentEvents.get(path).fold(false)(!_.isOverdue))
-            logger.debug(s"Ignoring watch event for $path due to anti-entropy constraint")
-          else
-            events.peek() match {
-              case Cancelled =>
-                logger.debug(s"Watch cancelled, not offering event for path $path")
-              case _ =>
-                recentEvents += path -> antiEntropy.fromNow
-                if (!events.offer(Triggered(path))) {
-                  logger.debug(s"Event already pending, dropping event for path: $path")
-                }
-            }
-        }
+      override def close(): Unit = monitor.close()
     }
   }
+
   // Shutup the compiler about unused arguments
   @inline private[this] def ignoreArg(arg: => Any): Unit = if (true) () else { arg; () }
   trait Logger {
     def debug(msg: => Any): Unit = ignoreArg(msg)
   }
-  object NullLogger extends Logger
-  private def newUserInputThread(terminationCondition: => Boolean,
-                                 events: ArrayBlockingQueue[Event],
-                                 logger: Logger): Looper =
-    new Looper(s"watch-state-user-input-${userInputId.incrementAndGet}") {
-      override final def loop(): Unit = {
-        Thread.sleep(10)
-        if (terminationCondition) {
-          logger.debug("Received termination condition. Stopping watch...")
-          events.peek match {
-            case Cancelled =>
-            case _ =>
-              while (!events.offer(Cancelled)) {
-                events.clear()
-              }
-          }
-        } else {}
-      }
-    }
-
-  private abstract class Looper(name: String) extends Thread(name) with AutoCloseable {
-    private[this] var stopped = false
-    private[this] var started = false
-    private[this] val lock = new Object()
-    def isStopped: Boolean = this.synchronized(stopped)
-    def loop(): Unit
-    @tailrec
-    private final def runImpl(firstTime: Boolean): Unit = {
-      if (firstTime) {
-        started = true
-        lock.synchronized(lock.notifyAll)
-      }
-      try {
-        if (!isStopped) {
-          loop()
-        }
-      } catch {
-        case (_: ClosedWatchServiceException | _: InterruptedException) =>
-          this.synchronized { stopped = true }
-      }
-      if (!isStopped) {
-        runImpl(firstTime = false)
-      }
-    }
-    override final def run(): Unit = runImpl(firstTime = true)
-    def close(): Unit = this.synchronized {
-      if (!stopped) {
-        stopped = true
-        this.interrupt()
-        this.join(5000)
-      }
-    }
-    setDaemon(true)
-    start()
-    lock.synchronized { if (!started) lock.wait() }
+  object NullLogger extends Logger {
+    override def debug(msg: => Any): Unit = ignoreArg(msg)
   }
-  private val eventThreadId = new AtomicInteger(0)
-  private val userInputId = new AtomicInteger(0)
-
 }
---
+++ io/src/main/scala/sbt/internal/io/WatchServiceBackedObservable.scala
@@ -0,0 +1,162 @@
+package sbt.internal.io
+
+import java.nio.file.StandardWatchEventKinds.OVERFLOW
+import java.nio.file.{ Files, Path, WatchKey }
+import java.util.concurrent.atomic.{ AtomicBoolean, AtomicInteger }
+import java.util.concurrent.{ CountDownLatch, TimeUnit }
+
+import sbt.io.FileTreeDataView.{ Entry, Observable }
+import sbt.io.FileTreeView.AllPass
+import sbt.io.{ FileTreeDataView, FileTreeView, Logger, TypedPath }
+
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.concurrent.duration._
+
+object WatchServiceBackedObservable {
+  private val eventThreadId = new AtomicInteger(0)
+}
+import sbt.internal.io.WatchServiceBackedObservable._
+private[sbt] class WatchServiceBackedObservable[+T](s: WatchState,
+                                                    delay: FiniteDuration,
+                                                    converter: TypedPath => T,
+                                                    closeService: Boolean,
+                                                    logger: Logger)
+    extends Observable[T] {
+  private[this] val closed = new AtomicBoolean(false)
+  private[this] val observers = new Observers[T]
+  private[this] val thread: Thread = {
+    val entryConverter = Entry.converter(converter)
+    var registered = s.registered
+    val lock = new Object
+    val latch = new CountDownLatch(1)
+    new Thread(s"watch-state-event-thread-${eventThreadId.incrementAndGet()}") {
+      setDaemon(true)
+      start()
+      if (!latch.await(5, TimeUnit.SECONDS))
+        throw new IllegalStateException("Couldn't start event thread")
+      @tailrec
+      final def loopImpl(): Unit = {
+        if (!closed.get) getFilesForKey(s.service.poll(delay)).foreach { entry =>
+          if (entry.typedPath.exists) observers.onCreate(entry) else observers.onDelete(entry)
+        }
+        if (!closed.get) loopImpl()
+      }
+      override def run(): Unit = {
+        latch.countDown()
+        try {
+          loopImpl()
+        } catch {
+          case _: InterruptedException =>
+        }
+      }
+
+      def getFilesForKey(key: WatchKey): Vector[Entry[T]] = key match {
+        case null => Vector.empty
+        case k =>
+          val rawEvents = k.synchronized {
+            val events = k.pollEvents.asScala.toVector
+            k.reset()
+            events
+          }
+          val keyPath = k.watchable.asInstanceOf[Path]
+          val allEvents = rawEvents.flatMap {
+            case e if e.kind.equals(OVERFLOW) =>
+              handleOverflow(k)
+            case e if !e.kind.equals(OVERFLOW) && e.context != null =>
+              Some(TypedPath(keyPath.resolve(e.context.asInstanceOf[Path])))
+            case _ => None
+          }
+          logger.debug(s"Received events:\n${allEvents.mkString("\n")}")
+          val (exist, notExist) = allEvents.partition(_.exists)
+          val (updatedDirectories, updatedFiles) = exist.partition(_.isDirectory)
+          val newFiles = updatedDirectories.flatMap(filesForNewDirectory)
+          lock.synchronized { registered --= notExist.map(_.getPath) }
+          notExist.foreach(p => s.unregister(p.getPath))
+          (updatedDirectories ++ updatedFiles ++ newFiles ++ notExist).map(tp =>
+            Entry(tp, entryConverter))
+      }
+
+      /*
+       * In the case of an overflow, we must poll the file system to find out if there are added
+       * or removed directories. When there are new directories, we also want to return file
+       * events for the files that are found therein. Because an overflow is likely to occur while
+       * a directory is still being modified, we poll repeatedly until we get the same list of
+       * files consecutively. We will not trigger for any files that are updated while the WatchKey
+       * is in the OVERFLOW state. There is no good way to fix this without caching mtimes for
+       * all of the files, which I don't think is worth doing at this juncture.
+       */
+      private def handleOverflow(key: WatchKey): Vector[TypedPath] = lock.synchronized {
+        val allFiles = new mutable.HashSet[TypedPath]
+        def addNewFiles(): Unit = {
+          allFiles.clear()
+          val path = key.watchable.asInstanceOf[Path]
+          val view = FileTreeView.DEFAULT
+          view.list(path, maxDepth = Integer.MAX_VALUE, AllPass).foreach { typedPath =>
+            allFiles += typedPath
+            val path = typedPath.getPath
+            if (typedPath.isDirectory && !registered.contains(path)) {
+              registered += path -> s.register(path)
+            }
+          }
+          ()
+        }
+
+        var oldFiles = mutable.Set.empty[TypedPath]
+        do {
+          oldFiles = allFiles
+          addNewFiles()
+        } while (oldFiles != allFiles)
+        registered --= registered.collect {
+          case (d, k) if !Files.exists(d) =>
+            k.reset()
+            k.cancel()
+            d
+        }
+        allFiles.toVector
+      }
+
+      /*
+       * Returns new files found in new directory and any subdirectories, assuming that there is
+       * a recursive source with a base that is parent to the directory.
+       */
+      private def filesForNewDirectory(typedPath: TypedPath): Seq[TypedPath] =
+        if (!closed.get()) {
+          val dir = typedPath.getPath
+          lazy val recursive =
+            s.sources.exists(src => dir.startsWith(src.base.toPath) && src.recursive)
+          if (!registered.contains(dir) && recursive) {
+            registered += dir -> s.register(dir)
+            @tailrec
+            def poll(paths: Seq[Path] = Nil): Seq[TypedPath] = {
+              val typedPaths = FileTreeView.DEFAULT.list(dir, maxDepth = Integer.MAX_VALUE, AllPass)
+              val newPaths = typedPaths.map(_.getPath)
+              if (newPaths == paths) typedPaths else poll(newPaths)
+            }
+            val result = poll()
+            val newDirs = result.collect {
+              case tp if tp.isDirectory && !closed.get() => tp.getPath -> s.register(tp.getPath)
+            }
+            lock.synchronized { registered ++= newDirs }
+            result.toVector
+          } else Nil
+        } else Nil
+    }
+  }
+  override def addObserver(observer: FileTreeDataView.Observer[T]): Int =
+    observers.addObserver(observer)
+
+  override def removeObserver(handle: Int): Unit = {
+    observers.removeObserver(handle)
+    ()
+  }
+  override def close(): Unit = {
+    if (closed.compareAndSet(false, true)) {
+      thread.interrupt()
+      thread.join(5.seconds.toMillis)
+      if (closeService) s.close()
+      logger.debug("Closed WatchServiceBackedObservable")
+    }
+  }
+}
