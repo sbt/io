14a68b71d7912a846df4455f05dfc291f82791e4
xuwei-k <6b656e6a69@gmail.com>
2018-06-17 00:07:47 +0900

    enable Scala 2.13.0-M4 test

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,12 @@ object Dependencies {
   val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
 
   val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.5-M1"
+  val scalatest = Def.setting {
+    if (scalaVersion.value == "2.13.0-M4")
+      "org.scalatest" %% "scalatest" % "3.0.6-SNAP1"
+    else
+      "org.scalatest" %% "scalatest" % "3.0.5-M1"
+  }
   val jna = "net.java.dev.jna" % "jna" % "4.5.0"
   val jnaPlatform = "net.java.dev.jna" % "jna-platform" % "4.5.0"
   val appleFileEvents = "com.swoval" % "apple-file-events" % "1.3.2"
