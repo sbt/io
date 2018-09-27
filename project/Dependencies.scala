ed247d93995241a37bab491dec67f8d90321acf8
Eugene Yokota <eed3si9n@gmail.com>
2018-09-27 13:17:25 -0400

    Scala 2.12.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   val scala210 = "2.10.7"
   val scala211 = "2.11.12"
-  val scala212 = "2.12.6"
+  val scala212 = "2.12.7"
   val scala213 = "2.13.0-M4"
 
   val scalaCompiler = Def.setting { "org.scala-lang" % "scala-compiler" % scalaVersion.value }
