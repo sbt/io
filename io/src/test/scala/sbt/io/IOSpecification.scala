2874bc1139548f4be25e6e131573df6806fd955e
Eugene Yokota <eed3si9n@gmail.com>
2018-09-28 01:40:42 -0400

    Test needs to work on JDK 8 too

--- io/src/test/scala/sbt/io/IOSpecification.scala
+++ io/src/test/scala/sbt/io/IOSpecification.scala
@@ -2,18 +2,15 @@ package sbt.io
 
 import scala.util.Try
 import org.scalacheck._, Prop._
-import java.net.URI
-import java.nio.file.{ FileSystems, Files }
+import java.nio.file.Files
 
 object IOSpecification extends Properties("IO") {
-  private lazy val jrtFs = FileSystems.getFileSystem(URI.create("jrt:/"))
-
   property("IO.classLocationPath able to determine containing directories") = forAll(classes) {
     (c: Class[_]) =>
       Try(IO.classLocationPath(c)).toOption.exists {
         case jar if jar.getFileName.toString.endsWith(".jar") =>
           Files.isRegularFile(jar)
-        case jrt if jrt.getFileSystem == jrtFs =>
+        case jrt if jrt.toUri.getScheme == "jrt" =>
           jrt.toString.contains("/java.base")
         case dir =>
           Files.isDirectory(dir)
