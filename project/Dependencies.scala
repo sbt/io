11756c96a53fe33b8bd101e4456a663ce4b34e3e
Ethan Atkins <ethan.atkins@gmail.com>
2019-07-08 15:26:37 -0700

    Bump swoval
    The updated swoval version removes the native libraries for 32 bit
    platforms because there are no tests for them anywhere. I also made it
    more explicit about for which platforms native libraries exists. For
    platforms where there are no native libraries are provided, the swoval
    library should safely fall back to jvm built ins.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,6 +11,6 @@ object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
   val jna = "net.java.dev.jna" % "jna" % "4.5.0"
   val jnaPlatform = "net.java.dev.jna" % "jna-platform" % "4.5.0"
-  val swovalFiles = "com.swoval" % "file-tree-views" % "2.1.2"
+  val swovalFiles = "com.swoval" % "file-tree-views" % "2.1.3"
   def nightlyVersion: Option[String] = sys.props.get("sbt.build.version")
 }
