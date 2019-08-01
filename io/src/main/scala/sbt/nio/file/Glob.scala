a4334d62e0883b36abbf52ce1fb1ce34bf93c129
Ethan Atkins <contact@ethanatkins.com>
2019-08-01 11:48:43 -0700

    Add scaladoc for Glob
    There were no scaladocs for the Glob api. This commit adds scaladoc for
    all of the public methods and values in Glob.scala and moves a few apis
    to be package private.

--- io/src/main/scala/sbt/nio/file/Glob.scala
+++ io/src/main/scala/sbt/nio/file/Glob.scala
@@ -21,6 +21,53 @@ import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 import scala.util.Properties
 
+/**
+ * Represents a query for a path on the file system. Instances of `Glob` can be combined together
+ * to build a more complicated query. Similar to the path name components of a `Path`, a `Glob` has
+ * components and matching is done from left to right component-wise. Each component is matched
+ * using a
+ * [[https://docs.oracle.com/javase/8/docs/api/java/nio/file/FileSystem.html#getPathMatcher-java.lang.String-
+ * PathMatcher]].
+ * For example,
+ * {{{
+ *   val baseGlob = Glob("/foo/bar")
+ *   baseGlob.matches(Paths.get("/foo/bar")) // true
+ *   baseGlob.matches(Paths.get("/foo")) // false
+ *   baseGlob.matches(Paths.get("/foo/bar/baz")) // false
+ *
+ *   val children = baseGlob / AnyPath
+ *   children.matches(Paths.get("/foo/bar")) // false
+ *   children.matches(Paths.get("/foo/bar/baz")) // true
+ *   children.matches(Paths.get("/foo/bar/baz/buzz")) false
+ *
+ *   val secondGenerationChildren = children / AnyPath
+ *   secondGeneration.matches(Paths.get("/foo/bar/baz")) // false
+ *   secondGeneration.matches(Paths.get("/foo/bar/baz/buzz")) // true
+ *   secondGeneration.matches(Paths.get("/foo/bar/baz/buzz/fizz")) // false
+ * }}}
+ * where [[AnyPath]] matches any path name.
+ *
+ * Globs can be recursive and are constructed using the [[RecursiveGlob]]:
+ * {{{
+ *   val baseGlob = Glob("/foo/bar")
+ *   val allDescendants = baseGlob / RecursiveGlob
+ *   allDescendants.matches(Paths.get("/foo/bar")) // false
+ *   allDescendants.matches(Paths.get("/foo/bar/baz/buzz.txt")) // true
+ * }}}
+ *
+ * They can also filter on the basis file names:
+ * {{{
+ *   val baseGlob = Glob("/foo/bar")
+ *   val allScalaSources = baseGlob / RecursiveGlob / "*.scala"
+ *   allScalaSources.matches(Paths.get("/foo/bar/Foo.scala")) // true
+ *   allScalaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Bar.scala")) // true
+ *   allScalaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Buzz.java")) // false
+ *
+ *   val allScalaAndJavaSources = baseGlob / RecursiveGlob / "*.{java,scala}"
+ *   allScalaAndJavaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Bar.scala")) // true
+ *   allScalaAndJavaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Buzz.java")) // true
+ * }}}
+ */
 sealed trait Glob {
 
   /**
@@ -35,12 +82,84 @@ sealed trait Glob {
 object Glob {
   private[this] def comp[T](left: T, right: T)(implicit ordering: Ordering[T]): Int =
     ordering.compare(left, right)
+
+  /**
+   * Construct a single file [[Glob]]. The resulting [[Glob]] will only match the input file.
+   * @param file the single path to match.
+   * @return a Glob whose [[Glob.matches]] method only accepts the input path.
+   */
   def apply(file: File): Glob = if (file.isAbsolute) Root(file.toPath) else apply(file.toString)
+
+  /**
+   * Construct a Glob with a file prefix and relativeGlob glob string. For example:
+   * {{{
+   *   val glob = Glob(file("foo/bar"), "*.txt") // matches all txt files in foo/bar
+   *   val recursiveGlob = Glob(file("foo/bar"), "**<code>/</code>*.txt") // recursively matches all text files in foo/bar
+   * }}}
+   * @param file the prefix of the glob pattern. This can be an absolute or relativeGlob file.
+   * @param relative the relativeGlob glob string
+   * @return a Glob that matches the file prefix with
+   */
   def apply(file: File, relative: String): Glob = Glob(file, RelativeGlob.parse(relative))
-  def apply(file: File, relative: RelativeGlob): Glob = Glob(file.toPath) / relative
+
+  /**
+   * Construct a [[Glob]] with a file prefix and a [[RelativeGlob]] suffix. In order for a path to
+   * match the output [[Glob]], it must start with `file` and its relative path with respect to
+   * `file` must match the `relativeGlob`. For example:
+   * {{{
+   *   val txtFiles = Glob(file("foo/bar"), * / "*.txt") // matches all text files in any subdirectory of foo/bar
+   *   val allChildren = Glob(file("/foo/bar"), **) // matches all descendants of /foo/bar
+   *   val scalaSources = Glob(file("src/main/scala", ** / "*.scala")) // matches scala source file
+   * }}}
+   *
+   * @param file the prefix of the glob pattern. This can be an absolute or relativeGlob file.
+   * @param relativeGlob the [[RelativeGlob]] to append to the file prefix
+   * @return a Glob that matches a path with the same prefix as `file` and whose suffix matches
+   *         `relativeGlob`.
+   */
+  def apply(file: File, relativeGlob: RelativeGlob): Glob = Glob(file.toPath) / relativeGlob
+
+  /**
+   * Construct a single path Glob. The resulting glob
+   * @param path the single path to match.
+   * @return a Glob whose [[Glob.matches]] method only accepts the input path.
+   */
   def apply(path: Path): Glob = if (path.isAbsolute) Root(path) else apply(path.toString)
-  def apply(path: Path, relative: RelativeGlob): Glob = Glob(path) / relative
+
+  /**
+   * Construct a Glob with a path prefix and a `RelativeGlob`. For example:
+   * {{{
+   *   val txtFiles = Glob(Paths.get("foo/bar"), * / "*.txt") // matches all text files in any subdirectory of foo/bar
+   *   val allChildren = Glob(Paths.get("/foo/bar"), **) // matches all descendants of /foo/bar
+   *   val scalaSources = Glob(Paths.get("src/main/scala", ** / "*.scala")) // matches scala source file
+   * }}}
+   * @param path the prefix of the glob pattern. This can be an absolute or relativeGlob file.
+   * @param relativeGlob the [[RelativeGlob]] to append to the file prefix
+   * @return a Glob that matches the file prefix with
+   */
+  def apply(path: Path, relativeGlob: RelativeGlob): Glob = Glob(path) / relativeGlob
+
+  /**
+   * Construct a Glob with a file prefix and relativeGlob glob string. For example:
+   * {{{
+   *   val glob = Glob(file("foo/bar"), "*.txt") // matches all text files in foo/bar
+   * }}}
+   * @param path the prefix of the glob pattern. This can be an absolute or relativeGlob file.
+   * @param relative the relativeGlob glob string
+   * @return a Glob that matches the file prefix with
+   */
   def apply(path: Path, relative: String): Glob = Glob(path) / relative
+
+  /**
+   * Construct a glob from a string. It can represent a full path. Any `/` characters in a pathname
+   * are automatically converted to `\\` on windows.
+   * {{{
+   *   val absoluteGlob = Glob("/foo/bar<code>/</code>**<code>/</code>*.txt")
+   *   val relativeGlob = Glob("*<code>/</code>*.txt")
+   * }}}
+   * @param glob the string to convert to a glob
+   * @return a parsed `Glob` for the input. May throw if the input cannot be parsed into a `Glob`.
+   */
   def apply(glob: String): Glob = {
     val parts = splitter(glob)
     @tailrec def fullGlob(path: Path, rest: List[String]): Glob = {
@@ -64,6 +183,11 @@ object Glob {
       case _ => RelativeGlob(parts: _*)
     }
   }
+
+  /**
+   * Provides a default ordering for glob. Globs with a path prefix precede relative globs and are
+   * sorted by their path prefix.
+   */
   implicit object ordering extends Ordering[Glob] {
     override def compare(left: Glob, right: Glob): Int = left match {
       case Pattern(leftRoot, leftRelative) =>
@@ -168,6 +292,11 @@ object Glob {
     s"$prefix: Tried to extract the base path for relative glob $relative. $action" +
       s"re-run the program with java option, -D${RelativeGlobViewOption.propName}=allow"
   }
+
+  /**
+   * Provides extension methods for [[Glob]].
+   * @param glob the [[Glob]] to augment.
+   */
   implicit class GlobOps(val glob: Glob) extends AnyVal {
     private[sbt] def descendentMatches(path: Path): Boolean = glob match {
       case Pattern(p, relative) if path.startsWith(p) => relative.matches(p.getFileName)
@@ -216,26 +345,87 @@ object Glob {
       case relative: RelativeGlob             => RelativeGlob.range(relative)
       case FullFileGlob(_, recursive, _)      => if (recursive) (1, Int.MaxValue) else (1, 1)
     }
-    def /(glob: String): Glob = /(RelativeGlob.parse(glob))
-    def /(that: RelativeGlob): Glob = glob match {
-      case Pattern(root, relative) => Pattern(root, relative / that)
+
+    /**
+     * Appends a relative glob path to an existing [[Glob]]. For example:
+     * {{{
+     *   val glob = Glob("foo/bar")
+     *   val scalaSources = glob / "**<code>/</code>*.scala"
+     *   scalaSources.matches(Paths.get("foo/bar/Foo.scala")) // true
+     *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Bar.scala")) // true
+     *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Baz.java")) // false
+     * }}}
+     *
+     * @param relativeGlob the string that will be parsed into a [[RelativeGlob]] and appended to
+     *                     the base glob
+     * @return a Glob with the new [[RelativeGlob]] appended to the query.
+     */
+    def /(relativeGlob: String): Glob = /(RelativeGlob.parse(relativeGlob))
+
+    /**
+     * Appends a relative glob path to an existing [[Glob]]. For example:
+     * {{{
+     *   val glob = Glob("foo/bar")
+     *   val scalaSources = glob / "**<code>/</code>*.scala"
+     *   scalaSources.matches(Paths.get("foo/bar/Foo.scala")) // true
+     *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Bar.scala")) // true
+     *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Baz.java")) // false
+     * }}}
+     *
+     * @param relativeGlob the [[RelativeGlob]] to be appended to the query
+     * @return a Glob with the new [[RelativeGlob]] appended to the query.
+     */
+    def /(relativeGlob: RelativeGlob): Glob = glob match {
+      case Pattern(root, relative) => Pattern(root, relative / relativeGlob)
       case Root(path) =>
-        val newRoot = that.prefix.map(path.resolve).getOrElse(path)
-        that.tail match {
+        val newRoot = relativeGlob.prefix.map(path.resolve).getOrElse(path)
+        relativeGlob.tail match {
           case Nil => Root(newRoot)
           case t   => Pattern(newRoot, RelativeGlob(t))
         }
       case f: FullFileGlob =>
         throw new IllegalArgumentException(s"Can't call / on legacy glob $f")
-      case r: RelativeGlob => r / that
+      case r: RelativeGlob => r / relativeGlob
     }
   }
+
+  /**
+   * Converts a string to a [[Glob]].
+   * @param glob the string to convert
+   * @return the converted [[Glob]].
+   */
   implicit def stringToGlob(glob: String): Glob = Glob(glob)
+
+  /**
+   * Provides extension methods to `java.nio.file.Path`.
+   * @param path the `Path` instance to extend.
+   */
   final class PathOps(val path: Path) extends AnyVal {
+
+    /**
+     * Converts the path to a single path glob
+     * @return the single path [[Glob]].
+     */
     def toGlob: Glob = Root(path)
+
+    /**
+     * Appends a path component to a path.
+     * @param component the component to append
+     * @return the appended path.
+     */
     def /(component: String): Path = path.resolve(component)
   }
+
+  /**
+   * Provides extension methods to `java.nio.file.Path`
+   * @param file the `File` instance to extend.
+   */
   final class FileOps(val file: File) extends AnyVal {
+
+    /**
+     * Converts the file to a single path glob.
+     * @return the single path [[Glob]].
+     */
     def toGlob: Glob = Root(file.toPath)
   }
   private[this] val windowsEscapable = "(){}"
@@ -289,6 +479,14 @@ object Glob {
   }
 }
 
+/**
+ * A specialization of [[Glob]] that applies only to relative paths. [[RelativeGlob]] instances
+ * can always be combined with the `/` operator whereas general `Glob` instances cannot because they
+ * may correspond to an absolute file path:
+ * {{{
+ *   val combined = Glob("/foo/bar") / RelativeGlob("
+ * }}}
+ */
 sealed trait RelativeGlob extends Glob {
   private[file] def matchers: List[RelativeGlob.Matcher]
   private[file] def prefix: Option[Path] = matchers.takeWhile(_.isInstanceOf[PathComponent]) match {
@@ -299,30 +497,97 @@ sealed trait RelativeGlob extends Glob {
   }
   private[file] def tail: List[RelativeGlob.Matcher] =
     matchers.dropWhile(_.isInstanceOf[PathComponent])
-  def /(component: String): RelativeGlob = /(RelativeGlob.parse(component))
-  def /(that: RelativeGlob): RelativeGlob = RelativeGlob(this.matchers ::: that.matchers)
+
+  /**
+   * Appends additional components to this [[RelativeGlob]]:
+   * {{{
+   *   val glob = Glob("foo/bar")
+   *   val scalaSources = glob / "**<code>/</code>*.scala"
+   *   scalaSources.matches(Paths.get("foo/bar/Foo.scala")) // true
+   *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Bar.scala")) // true
+   *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Baz.java")) // false
+   * }}}
+   *
+   * @param relativeGlob the additional query string to parse and append
+   * @return a Glob with the new [[RelativeGlob]] appended to the query.
+   */
+  def /(relativeGlob: String): RelativeGlob = /(RelativeGlob.parse(relativeGlob))
+
+  /**
+   * Appends an additional [[RelativeGlob]] to this [[RelativeGlob]]:
+   * {{{
+   *   val glob = Glob("foo/bar")
+   *   val scalaSources = glob / ** / "*.scala"
+   *   scalaSources.matches(Paths.get("foo/bar/Foo.scala")) // true
+   *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Bar.scala")) // true
+   *   scalaSources.matches(Paths.get("foo/bar/baz/fizz/buzz/Baz.java")) // false
+   * }}}
+   *
+   * @param relativeGlob the additional query string to parse and append
+   * @return a Glob with the new [[RelativeGlob]] appended to the query.
+   */
+  def /(relativeGlob: RelativeGlob): RelativeGlob =
+    RelativeGlob(this.matchers ::: relativeGlob.matchers)
 }
+
+/**
+ * A [[RelativeGlob]] that matches any path. Can be combined with other [[Glob]] instances to build
+ * recursive queries:
+ * {{{
+ *   val scalaSources = Glob("/foo/bar") / RecursiveGlob / "*.scala"
+ *   scalaSources.matches(Paths.get("/foo/bar/Foo.scala")) // true
+ *   scalaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Bar.scala")) // true
+ * }}}
+ */
 case object RecursiveGlob extends SingleComponentMatcher with RelativeGlob {
-  override def glob: String = "**"
+  override private[file] def glob: String = "**"
   def matches(path: Path): Boolean = true
 }
+
+/**
+ * A [[RelativeGlob]] that matches any single path name.
+ * {{{
+ *   AnyPath.matches(Paths.get("foo")) // true
+ *   AnyPath.matches(Paths.get("foo/bar")) // false
+ *   Glob("foo/bar", AnyPath).matches(Paths.get("foo/bar")) // false
+ *   Glob("foo/bar", AnyPath).matches(Paths.get("foo/bar/baz")) // true
+ *   Glob("foo/bar", AnyPath).matches(Paths.get("foo/bar/baz/buzz")) // false
+ * }}}
+ *
+ * It can be used to specify the minimum depth of a query:
+ * {{{
+ *   val childScalaSources = Glob("/foo/bar") / AnyPath / RecursiveGlob / "*.scala"
+ *   childScalaSources.matches(Paths.get("/foo/bar/Foo.scala")) // false
+ *   childScalaSources.matches(Paths.get("/foo/bar/baz/Bar.scala")) // true
+ *   childScalaSources.matches(Paths.get("/foo/bar/baz/fizz/buzz/Bar.scala")) // true
+ * }}}
+ */
 case object AnyPath extends SingleComponentMatcher with RelativeGlob {
-  override def glob: String = "*"
+  override private[file] def glob: String = "*"
   override def matches(path: Path): Boolean = path.getNameCount == 1
 }
 object RelativeGlob {
+
+  /**
+   * Aliases [[RecursiveGlob]].
+   */
   val ** = RecursiveGlob
+
+  /**
+   * Aliases [[AnyPath]].
+   */
   val * = AnyPath
-  def parse(glob: String): RelativeGlob = Glob(glob) match {
+  private[file] def parse(glob: String): RelativeGlob = Glob(glob) match {
     case r: RelativeGlob => r
     case _ =>
-      throw new IllegalArgumentException(s"Couldn't create relative glob from absolute glob: $glob")
+      val msg = s"Couldn't create relativeGlob glob from absolute glob: $glob"
+      throw new IllegalArgumentException(msg)
   }
-  def apply(matchers: String*): RelativeGlob =
+  private[sbt] def apply(matchers: String*): RelativeGlob =
     new RelativeGlobImpl(matchers.view.filterNot(_ == ".").map(Matcher.apply).toList)
   private[sbt] def apply(matchers: List[Matcher]): RelativeGlob = new RelativeGlobImpl(matchers)
   implicit val ordering: Ordering[RelativeGlob] = Ordering.by(_.matchers)
-  private[file] def range(relative: RelativeGlob) = {
+  private[file] def range(relative: RelativeGlob): (Int, Int) = {
     val res = relative.matchers.foldLeft((0, 0)) {
       case ((0, 0), RecursiveGlob)                => (1, Int.MaxValue)
       case (r @ (_, Int.MaxValue), RecursiveGlob) => r
@@ -374,7 +639,7 @@ object RelativeGlob {
 
   private[sbt] sealed trait Matcher extends RelativeGlob
   private[sbt] object Matcher {
-    implicit object ordering extends Ordering[Matcher] {
+    private[sbt] implicit object ordering extends Ordering[Matcher] {
       override def compare(x: Matcher, y: Matcher): Int = x match {
         case RecursiveGlob =>
           y match {
@@ -422,7 +687,7 @@ object RelativeGlob {
           }
       }
     }
-    implicit val listOrdering: Ordering[List[Matcher]] = new Ordering[List[Matcher]] {
+    private[sbt] implicit val listOrdering: Ordering[List[Matcher]] = new Ordering[List[Matcher]] {
       override def compare(left: List[Matcher], right: List[Matcher]): Int = {
         val leftIt = left.iterator
         val rightIt = right.iterator
@@ -445,22 +710,22 @@ object RelativeGlob {
       case AnyPath => NoPath
       case m       => NotMatcher(m)
     }
-    def apply(glob: String): Matcher = glob match {
+    private[sbt] def apply(glob: String): Matcher = glob match {
       case "**"                   => RecursiveGlob
       case "*"                    => AnyPath
       case g if g.startsWith("!") => NotMatcher(Matcher(g.drop(1)))
       case g if !Glob.hasMeta(g)  => PathComponent(g)
       case g                      => new GlobMatcher(g)
     }
-    def apply(f: String => Boolean): Matcher = FunctionNameFilter(f)
+    private[sbt] def apply(f: String => Boolean): Matcher = FunctionNameFilter(f)
   }
   private[sbt] case object NoPath extends SingleComponentMatcher with RelativeGlob {
     override def glob: String = "<null>"
     override def matches(path: Path): Boolean = false
   }
   private[file] sealed trait SingleComponentMatcher extends Matcher {
-    def glob: String
-    override final val matchers: List[Matcher] = this :: Nil
+    private[file] def glob: String
+    override private[file] final val matchers: List[Matcher] = this :: Nil
     override def toString: String = glob
   }
   private[file] object PathComponent {
