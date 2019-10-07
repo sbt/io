f0a3ffc262d1cba0cd82e2c807ea2f3c974f240e
Ethan Atkins <contact@ethanatkins.com>
2019-10-07 14:24:42 -0700

    Fix IO.getModifiedTimeOrZero for unnormalized paths
    The fix for long path names on windows,
    099aa4f3ced649b48a9cf6595fbdb5524b69cfa5, had the unintended side effect
    of causing IO.getModifiedTimeOrZero to throw uncaught IOExceptions when
    the path name was not normalized. Specifically, it would throw if the
    path was empty or if it had relative components. To fix this, we try to
    get the modified time but if the file is not found, we normalize the
    path and make it absolute and retry.
    
    This change was primarily made for windows, but it turns out that
    IO.getModifiedTimeOrZero(file("")) also didn't work correctly. Naively,
    I'd expect the empty path to be expanded to the program working
    directory but it would actually just return 0 because the posix native
    api did not handle empty strings either. Relative paths did work on
    posix before this change.

--- io/src/test/scala/sbt/io/LastModifiedSpec.scala
+++ io/src/test/scala/sbt/io/LastModifiedSpec.scala
@@ -10,9 +10,10 @@
 
 package sbt.io
 
-import java.nio.file.Files
+import java.nio.file.{ Files, Paths => JPaths }
 
 import org.scalatest.FlatSpec
+import sbt.nio.file.syntax._
 
 class LastModifiedSpec extends FlatSpec {
   "IO.getModifiedTimeOrZero" should "work with long path names" in IO.withTemporaryDirectory {
@@ -27,5 +28,23 @@ class LastModifiedSpec extends FlatSpec {
       val lm = (System.currentTimeMillis / 1000) * 1000
       IO.setModifiedTimeOrFalse(file, lm)
       assert(IO.getModifiedTimeOrZero(file) == lm)
+  }
+  it should "handle empty paths" in {
+    assert(IO.getModifiedTimeOrZero(JPaths.get("").toFile) > 0)
+    val newLM = ((System.currentTimeMillis + 10000) / 1000) * 1000
+    IO.setModifiedTimeOrFalse(JPaths.get("").toFile, newLM)
+    assert(IO.getModifiedTimeOrZero(JPaths.get("").toFile) == newLM)
+  }
+  it should "handle relative paths" in IO.withTemporaryDirectory { dir =>
+    val dirPath = dir.toPath
+    val subDir1 = Files.createDirectories(dirPath / "subdir-1")
+    val subDir2 = Files.createDirectories(dirPath / "subdir-2")
+    val subFile = Files.createFile(subDir2 / "file")
+    val lm = IO.getModifiedTimeOrZero(subFile.toFile)
+    val relative = subDir1 / ".." / subDir2.getFileName.toString / subFile.getFileName.toString
+    assert(IO.getModifiedTimeOrZero(relative.toFile) == lm)
+    val newLM = ((System.currentTimeMillis + 10000) / 1000) * 1000
+    IO.setModifiedTimeOrFalse(relative.toFile, newLM)
+    assert(IO.getModifiedTimeOrZero(relative.toFile) == newLM)
   }
 }
