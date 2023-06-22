d342c58d0c928b0ba90226995e515a888cae8ec1
xuwei-k <6b656e6a69@gmail.com>
2023-06-23 07:06:14 +0900

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 import Keys._
 
 object Dependencies {
-  val scala212 = "2.12.16"
-  val scala213 = "2.13.8"
-  val scala3 = "3.1.3"
+  val scala212 = "2.12.18"
+  val scala213 = "2.13.11"
+  val scala3 = "3.3.0"
 
   val scalaCompiler = Def.setting {
     val v = if (scalaBinaryVersion.value == "3") scala213 else scalaVersion.value
     "org.scala-lang" % "scala-compiler" % v
   }
 
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.15.4"
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.10"
-  val jnaVersion = "5.12.0"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.16"
+  val jnaVersion = "5.13.0"
   val jna = "net.java.dev.jna" % "jna" % jnaVersion
   val jnaPlatform = "net.java.dev.jna" % "jna-platform" % jnaVersion
   val swovalFiles = "com.swoval" % "file-tree-views" % "2.1.10"
