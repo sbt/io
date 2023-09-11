b3e4dbfe06eddd7ff5ef59d91df2bea21702edf6
Matthew de Detrich <mdedetrich@gmail.com>
2023-09-11 12:06:37 +0200

    Update scala versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import Keys._
 
 object Dependencies {
   val scala212 = "2.12.18"
-  val scala213 = "2.13.11"
-  val scala3 = "3.3.0"
+  val scala213 = "2.13.12"
+  val scala3 = "3.3.1"
 
   val scalaCompiler = Def.setting {
     val v = if (scalaBinaryVersion.value == "3") scala213 else scalaVersion.value
