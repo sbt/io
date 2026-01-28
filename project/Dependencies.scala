1b4b82dde01a78e17dafa17f4d40dd85a19f27ed
Scala Steward <scala_steward@virtuslab.com>
2026-01-28 06:37:27 +0000

    Update scala3-compiler, scala3-library to 3.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import Keys._
 object Dependencies {
   val scala212 = "2.12.21"
   val scala213 = "2.13.18"
-  val scala3 = "3.7.4"
+  val scala3 = "3.8.1"
 
   val scalaCompiler = Def.setting {
     val v = scalaVersion.value
