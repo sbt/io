1431eec5060619c838a7739bec3a0cf6c11f68e4
Scala Steward <scala_steward@virtuslab.com>
2026-05-07 17:08:10 +0000

    Update scala-compiler, scala-library to 2.13.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object Dependencies {
   val scala212 = "2.12.20"
-  val scala213 = "2.13.16"
+  val scala213 = "2.13.18"
   val scala3 = "3.3.6"
 
   val scalaCompiler = Def.setting {
