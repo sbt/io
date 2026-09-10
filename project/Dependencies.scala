3c0d816ed44a45518d76b837c64a36b93bba7fef
Scala Steward <scala_steward@virtuslab.com>
2026-09-10 23:32:46 +0000

    Update scalacheck to 1.20.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   }
 
   val scalaVerify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.19.0"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.20.0"
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.20"
   val swovalFiles = "com.swoval" % "file-tree-views" % "2.1.12"
 }
